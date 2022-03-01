203f5a74bc3cd8af99f4f7aa2fa65911133ff9f3
skrall3119 <alexjanci@yahoo.com>
2022-03-01 14:17:24 -0600

    init

--- TVWeatherApp/ViewController.swift
+++ TVWeatherApp/ViewController.swift
@@ -9,9 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var currTemp: UILabel!
+    @IBOutlet weak var currSum: UILabel!
+    @IBOutlet weak var currDew: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let currentDate = Date.now.formatted(date: .abbreviated, time: .omitted)
+        dateLabel.text = currentDate
+        
+        if let url = NSURL(string: "https://api.darksky.net/forecast/ae58c5fa7285b492f6a553d200018d9e/42.1916,88.3501"){
+            if let data = NSData(contentsOf: url as URL){
+                do {
+                    let parsed = try JSONSerialization.jsonObject(with: data as Data, options: JSONSerialization.ReadingOptions.allowFragments) as! [String: AnyObject]
+                    let newDict = parsed
+                    
+                    print(newDict["currently"]!["summary"] as Any)
+                    print(newDict["currently"]!["temperature"] as Any)
+                    print(newDict["currently"]!["dewPoint"] as Any)
+                    
+                    self.currTemp.text = "\(newDict["currently"]!["temperature"]!!)"
+                    self.currSum.text = "\(newDict["currently"]!["summary"]!!)"
+                    self.currDew.text = "\(newDict["currently"]!["dewPoint"]!!)"
+                }
+                catch let error as NSError{
+                    print(error)
+                }
+            }
+        }
     }
 
 
